386cb54553e771160b014bd3a10a023a823df987
katai5plate <katai5plate.sub@gmail.com>
2020-03-09 11:17:35 +0900

    add keys

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,28 @@ import Balloon from "./Balloon";
 
 const FC: React.FC = props => (
   <div>
-    <Balloon direction="left">
+    <Balloon key={1} direction="left">
       {[
         "サービスのご利用ありがとうございます。\nこちらは株式会社〇〇です。",
         "ご用件をご選択ください。"
       ]}
     </Balloon>
-    <Balloon direction="right">
-      {["製品に関するお問い合わせ", "営業部にご用", "経理部にご用"]}
+    <Balloon key={2} direction="right" isButtons={true}>
+      {[
+        "製品に関するお問い合わせ",
+        "営業部へのお問い合わせ",
+        "経理部へのお問い合わせ"
+      ]}
+    </Balloon>
+    <Balloon key={3} direction="right">
+      {["選択: 営業部へのお問い合わせ"]}
+    </Balloon>
+    <Balloon key={4} direction="left">
+      {[
+        "ご返答ありがとうございます。",
+        "営業部へのお問い合わせは、\n以下の電話番号にお問い合わせください。",
+        "000-123-4567"
+      ]}
     </Balloon>
   </div>
 );
--- src/Balloon.tsx
+++ src/Balloon.tsx
@@ -4,19 +4,29 @@ import "./Balloon.scss";
 interface Props {
   direction: "left" | "right";
   children: string[];
+  isButtons?: boolean;
 }
 
-const FC: React.FC<Props> = ({ children, direction }) => (
+const FC: React.FC<Props> = ({ children, direction, isButtons }) => (
   <div>
     <div className={["balloon", direction].join(" ")}>
-      {children.map(item => (
-        <p>
-          {item.split("\n").map((line, index, { length }) => (
-            <>
-              {line}
-              {index !== length - 1 ? <br /> : ""}
-            </>
-          ))}
+      {children.map((item, i) => (
+        <p key={i}>
+          {item.split("\n").map((line, index, { length }) => {
+            const Content: React.FC = () => (
+              <>
+                {line}
+                {index !== length - 1 ? <br /> : ""}
+              </>
+            );
+            return isButtons ? (
+              <button key={index}>
+                <Content />
+              </button>
+            ) : (
+              <Content key={index} />
+            );
+          })}
         </p>
       ))}
     </div>
